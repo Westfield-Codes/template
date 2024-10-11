1cff4b232da0817cf262c7ccc1997545dc24b97b
Martin Bram Moreinis <martin.moreinis@schoolsofwestfield.org>
2024-10-10 20:37:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
+/* Nim Trainer by [your name]
+ * based on this flowchart:
+ * https://lucid.app/lucidchart/2018baaf-4c26-4a76-a0d5-93c97f444425/view
+ */
 
+/** 
+ * main  
+ * Handles new Nim games with gametype choice simple or trainer and a play again option. 
+ * @param none 
+ * @return none
+ */
+
+// global variables
+
+
+/* Main */
+function main(){
+
+}
+
+/** 
+ * playNim 
+ * plays a game with user first and computer second. Winner declared in an alert box. 
+ * @param none 
+ * @return none
+ */
+
+/* playNim */
+function playNim(){
+
+}
+
+/** 
+ * userTurn  
+ * User enters a turn. Validation against cheating handled by recursion.
+ * @param none 
+ * @return none
+ */
+function userTurn(){
+
+}
+
+/** 
+ * cpuTurn 
+ * Generate computer's turn without losing on purpose.  Different turns if trainer or simple.  
+ * @param none 
+ * @return none
+ */
+function cpuTurn(){
+
+}
